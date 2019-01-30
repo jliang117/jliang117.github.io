af4c79f4bfefce0a57a61f99e176323207c638f5
jliang117 <jliang117@users.noreply.github.com>
2019-01-30 02:16:12 -0500

    maybe

--- push.sh
+++ push.sh
@@ -2,27 +2,15 @@
 #update json
 python3 scrape.py
 
-setup_git() {
+push() {
   echo 'entering setup'
-  git remote add origin https://${GH_TOKEN}github.com/jliang117/jliang117.github.io.git
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-  git config --global push.default simple
   git checkout master
-}
-
-commit_website_files() {
-  echo 'adding to commit'
   git add pins.json
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-  git status
+  git push -f -q https://jliang117:${GH_TOKEN}@github.com/jliang117/jliang117.github.io.git
+  
 }
 
-upload_files() {
-  echo 'pushing'
-  git push --set-upstream origin master
-}
-
-setup_git
-commit_website_files
-upload_files
+push
